4456d6029312dcf1ee3d6571dc36510db172ed71
Joe Corall <joe@libops.io>
2026-03-14 09:44:22 -0400

    Fix --version flag

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/libops/sitectl-isle/cmd"
 	"github.com/libops/sitectl/pkg/plugin"
+)
+
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 func main() {
 	sdk := plugin.NewSDK(plugin.Metadata{
 		Name:        "isle",
-		Version:     "v0.0.7",
+		Version:     fmt.Sprintf("%s (Built on %s from Git SHA %s)", version, date, commit),
 		Description: "Islandora (ISLE) utilities and migration tools",
 		Author:      "libops",
 	})
