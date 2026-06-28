c94b45ebd35f11d051a09d2b0a4563b8bd192d47
Joe Corall <joe@libops.io>
2026-06-28 22:02:34 +0000

    Use shared healthcheck helpers

--- cmd/healthcheck.go
+++ cmd/healthcheck.go
@@ -1,10 +1,6 @@
 package cmd
 
 import (
-	"context"
-	"net/url"
-	"strings"
-
 	"github.com/libops/sitectl/pkg/config"
 	"github.com/libops/sitectl/pkg/healthcheck"
 	"github.com/libops/sitectl/pkg/plugin"
@@ -24,69 +20,31 @@ func (isleHealthcheckRunner) Run(cmd *cobra.Command, ctx *config.Context) ([]sit
 	defer func() { _ = checker.Close() }()
 
 	results := []sitevalidate.Result{
-		checker.CheckHTTPFromContainerWithHostHeader(
+		checker.CheckHTTPRoute(
 			cmd.Context(),
 			"http:drupal",
 			"drupal",
-			"http://127.0.0.1/",
-			publicURLHost(healthcheck.PublicURLFromEnv(ctx, "http", "islandora.io")),
+			healthcheck.PublicURLFromEnv(ctx, "http", "islandora.io"),
 		),
 	}
 
 	results = append(results,
 		checker.CheckMariaDB(cmd.Context(), "mariadb"),
+		checker.CheckComposeServiceDependsOnHealthy(cmd.Context(), "drupal", "mariadb"),
 		checker.CheckSolrCore(cmd.Context(), "solr", "default"),
 	)
 
-	optionalChecks := []struct {
-		service string
-		name    string
-		url     string
-	}{
-		{service: "homarus", name: "http:homarus-healthcheck", url: "http://127.0.0.1:8080/healthcheck"},
-		{service: "houdini", name: "http:houdini-healthcheck", url: "http://127.0.0.1:8080/healthcheck"},
-		{service: "hypercube", name: "http:hypercube-healthcheck", url: "http://127.0.0.1:8080/healthcheck"},
+	optionalResults, err := checker.CheckOptionalHTTPServices(cmd.Context(),
+		healthcheck.OptionalHTTPServiceCheck{Service: "homarus", Name: "http:homarus-healthcheck", URL: "http://127.0.0.1:8080/healthcheck"},
+		healthcheck.OptionalHTTPServiceCheck{Service: "houdini", Name: "http:houdini-healthcheck", URL: "http://127.0.0.1:8080/healthcheck"},
+		healthcheck.OptionalHTTPServiceCheck{Service: "hypercube", Name: "http:hypercube-healthcheck", URL: "http://127.0.0.1:8080/healthcheck"},
+	)
+	if err != nil {
+		return nil, err
 	}
-	for _, check := range optionalChecks {
-		ready, err := optionalHTTPServiceReady(cmd.Context(), checker, check.service)
-		if err != nil {
-			return nil, err
-		}
-		if !ready {
-			continue
-		}
-		results = append(results, checker.CheckHTTPFromContainer(cmd.Context(), check.name, check.service, check.url))
-	}
+	results = append(results, optionalResults...)
 
 	return results, nil
 }
 
 var _ plugin.HealthcheckRunner = isleHealthcheckRunner{}
-
-func optionalHTTPServiceReady(ctx context.Context, checker *healthcheck.DockerChecker, service string) (bool, error) {
-	exists, err := checker.ServiceExists(ctx, service)
-	if err != nil || !exists {
-		return false, err
-	}
-	results, err := checker.CheckComposeServices(ctx, service)
-	if err != nil {
-		return false, err
-	}
-	for _, result := range results {
-		if result.Status != sitevalidate.StatusOK {
-			return false, nil
-		}
-	}
-	return len(results) > 0, nil
-}
-
-func publicURLHost(rawURL string) string {
-	parsed, err := url.Parse(strings.TrimSpace(rawURL))
-	if err != nil {
-		return "islandora.io"
-	}
-	if host := parsed.Hostname(); host != "" {
-		return host
-	}
-	return "islandora.io"
-}
